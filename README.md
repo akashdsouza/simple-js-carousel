f681009ea37092b2a2bded437b993fdf9080ef2a
akash.dsouza <akash.dsouza@zohocorp.com>
2019-08-26 22:43:10 +0530

    Initial commit - basic implementation and Readme

---
+++ README.md
@@ -0,0 +1,65 @@
+# Simple JS Carousel
+
+Before you reach out to a carousel, first consider if you really need it. There are plenty of compeling cases to not use a carousel.
+
+Download the script and include it in your html file.
+
+The script expects certain classes to be available by default. For the html, 
+
+```html
+<div class="carousel-container">
+  <div class="carousel-content">
+    <div class="carousel-slide">
+    ...
+    </div>
+    ...
+    <div class="carousel-indicators"></div>
+    <div class="carousel-indicators"></div>
+    ...
+    <div class="carousel-next"></div>
+    <div class="carousel-prev"></div>
+  </div>
+</div>
+```
+
+add the following script
+
+```js
+carousel = new SJCarousel()
+```
+
+and the following styles
+
+```css
+.carousel-content {
+  display: flex;
+  transition: transform 1s ease;
+}
+.carousel-slide {
+  flex: 0 0 auto;
+}
+```
+
+## Arguments
+
+The carousel accepts two arguments:
+
+### Container element selector
+Optional. The container element for the carousel. It defaults to `.carousel-container`. You can pass a custom selector or even a dom node.
+
+### Options
+- `autoScroll` - Boolean to automatically scroll the carousel as soon as it is rendered. Dafaults to `false`
+- `visibleCount` - The total number of slides that are visible at a time. Defaults to `1`
+- `keyboardListenerOnDoc` - Boolean indicating if the keyboard event listeners should be registered on the document object. Defaults to `false`
+
+## Keyboard navigation
+
+The keyboard listeners are registered on the parent carousel container. To trigger these, the parent container should be in focus. To make a `div` focussable, add attribute `tabindex=0` to the container.
+
+For cases, where it might make sense to simply attach the event listener to the document instead of the container, set `keyboardListenerOnDoc` to `true`
+
+## Why another carousel?
+
+Carousels are usually used once on a web page. However, there may be cases where a carousel would have to be removed based on user interactions. It is common to set `display: none` on the container element and call it a day. But, depending on your use case, this might lead to a bunch to event listeners which are never removed and start leaking memory.
+
+Simple JS Carousel provides an easy method to remove all event listeners attached on the carousel. The script also includes support for keyboard navigation.
---
+++ simple-js-carousel.js
@@ -0,0 +1,129 @@
+class SJCarousel {
+  indicatorListeners = []
+  constructor(el = '.carousel-container', {
+    autoScroll,
+    visibleCount,
+    keyboardListenerOnDoc
+  } = {}) {
+    this.carouselContainer = typeof el === 'string' ? document.querySelector(el) : el;
+    if (!this.carouselContainer) {
+      return;
+    }
+    this.carouselElements = Array.from(this.carouselContainer.querySelectorAll('.carousel-slide'));
+    this.containerWidth = this.carouselContainer.getBoundingClientRect().width;
+    this.carouselContent = this.carouselContainer.querySelector('.carousel-content');
+    this.activeElement = 0;
+    this.visibleCount = visibleCount || 1;
+    this.keyboardListenerOnDoc = keyboardListenerOnDoc;
+    this.setContentWidth();
+    this.goToElement(this.activeElement);
+    this.addIndicatorEvents();
+    this.setLeftRightNavigation();
+    this.addKeyboardEvents();
+    this.addResizeEventHandler();
+    this.removeResizeEventHandler();
+    if (autoScroll) {
+      this.autoScroll();
+    }
+  }
+  autoScroll() {
+    this.scrollTimeout = setTimeout(() => {
+      this.goToNext();
+      this.autoScroll();
+    }, 6000);
+  }
+  setContentWidth() {
+    this.carouselElements.forEach((el) => {
+      el.style.flexBasis = `${this.containerWidth / this.visibleCount}px`;
+    })
+  }
+  goToElement(position) {
+    this.activeElement = position;
+    this.carouselContent.style.transform = `translateX(${position * (this.containerWidth / this.visibleCount) * -1}px)`;
+  }
+  addIndicatorEvents() {
+    this.carouselIndicators = Array.from(this.carouselContainer.querySelectorAll('.carousel-indicators'));
+    this.carouselIndicators.forEach((indicator, index) => {
+      let cb = () => {
+        this.goToElement(index);
+      }
+      this.indicatorListeners.push(cb);
+      indicator.addEventListener('click', cb);
+    });
+  }
+  removeCarouselIndicatorEvents() {
+    this.carouselIndicators.forEach((indicator, index) => {
+      indicator.removeEventListener('click', this.indicatorListeners[index]);
+    })
+  }
+  goToPrev = () => {
+    let activeElement = this.activeElement;
+    let length = this.carouselElements.length;
+    this.goToElement((activeElement + length - 1) % length);
+  }
+  goToNext = () => {
+    let activeElement = this.activeElement;
+    let length = this.carouselElements.length;
+    this.goToElement((activeElement + 1) % length);
+  }
+  keydownHandler = ({ keyCode }) => {
+    if (keyCode === 37) {
+      // Left key
+      this.goToPrev();
+    } else if (keyCode === 39) {
+      // Right key
+      this.goToNext();
+    }
+  }
+  addKeyboardEvents() {
+    if (this.keyboardListenerOnDoc) {
+      document.addEventListener('keydown', this.keydownHandler);
+    } else {
+      this.carouselContainer.addEventListener('keydown', this.keydownHandler);
+    }
+  }
+  removeKeyBoardEvents() {
+    if (this.keyboardListenerOnDoc) {
+      document.removeEventListener('keydown', this.keydownHandler);
+    } else {
+      this.carouselContainer.removeEventListener('keydown', this.keydownHandler);
+    }
+  }
+  setLeftRightNavigation() {
+    let prev = document.querySelector('.carousel-prev');
+    let next = document.querySelector('.carousel-next');
+    prev.addEventListener('click', this.goToPrev);
+    next.addEventListener('click', this.goToNext);
+  }
+  removeLeftRightNavigation() {
+    let prev = document.querySelector('.carousel-prev');
+    let next = document.querySelector('.carousel-next');
+    prev.removeEventListener('click', this.goToPrev);
+    next.removeEventListener('click', this.goToNext);
+  }
+  removeEventListeners() {
+    this.removeCarouselIndicatorEvents();
+    this.removeKeyBoardEvents();
+    this.removeLeftRightNavigation();
+    this.removeResizeEventHandler();
+  }
+  windowResized = () => {
+    this.containerWidth = this.carouselContainer.getBoundingClientRect().width;
+    this.setContentWidth();
+    this.goToElement(this.activeElement);
+  }
+  addResizeEventHandler() {
+    window.addEventListener('resize', this.windowResized);
+  }
+  removeResizeEventHandler() {
+    window.addEventListener('resize', this.windowResized);
+  }
+}
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   carousel = new SJCarousel('.carousel-container', {
+//     autoScroll: true,
+//     visibleCount: 2,
+//     keyboardListenerOnDoc: true
+//   });
+// });
